76bbd0799858853f2f53f6a90f04f6d84653b5f2
CharlieBarker <zcbtark@ucl.ac.uk>
2017-11-15 17:15:33 +0000

    finished iterativeanalysis - I'm all set to gogit add profitscripts/H_ca.pft

--- learnding/iterativeanalysis.pl
+++ learnding/iterativeanalysis.pl
@@ -13,14 +13,77 @@
 #		perl analyseabYmod.pl [jobname] [-abYmodflags] [<optional> 2> 
 #		results/abyModSTDERR/jobname.txt] 
 #		Optional allows the printing of STDERR readout into seperate file.
+#		Don't forget to use -noopt
+#	        Ensure you are running the tests on martin-cs00 (i.e. use "ssh
+#		martin-cs00") to get on the right machine.
 #  
 #               
 #*************************************************************************
 
-#DON'T FORGET TO USE -noopt
+
 use strict;
 
-my $var = `perl analyseabYmod.pl normal 2> results/abyModSTDERR/normal.txt`; 
-my $var = `perl analyseabYmod.pl verbose -v=3 2> results/abyModSTDERR/verbose.txt`; 
 
 
+###########################################ENTER JOBS HERE###########################################
+#DON'T FORGET TO USE -noopt
+# Usage - perl analyseabYmod.pl [job_name] [-abYmodflags] [<optional> 2> results/abyModSTDERR/job_name.txt]  
+#I would recomend taking 
+#Ensure you are running the tests on martin-cs00 (i.e. use "ssh
+#martin-cs00") to get on the right machine.
+
+		
+
+my $job1 = "perl analyseabYmod.pl 1 -noopt"; 
+my $job2 = "perl analyseabYmod.pl 2 -v=3 -noopt"; 
+my $job3 = "";
+my $job4 = "";
+my $job5 = "";
+my $job6 = "";
+my $job7 = "";
+my $job8 = "";
+my $job9 = "";
+my $job10 = "";
+my $job11 = "";
+my $job12 = "";
+my $job13 = "";
+my $job14 = "";
+my $job15 = "";
+
+#####################################################################################################
+
+my @jobs = ($job1, $job2, $job3, $job4, $job5, $job6, $job7, $job8, $job9, $job10, $job11, $job12,
+	$job13, $job14, $job15);
+#foreach $job (@jobs)
+#{
+#	split
+#}
+#go through jobs running them while the total number of abymods that are running is less than 4
+foreach $job (@jobs)
+{
+    my $nRunning  = 0;
+    my $firstCall = 1;
+    do
+    {
+        # Check how many abymods are running
+        $nRunning = `ps auxwww | grep abymod.pl | grep -v grep | wc -l`;
+        if($firstCall)
+        {
+            $firstCall = 0;
+        }
+        else
+        {
+            sleep 5;
+        }
+    } while($nRunning >= 4);
+
+    StartJob($job);
+	
+}
+
+############################################SUBROUTINES############################################
+sub StartJob
+{
+	my ($job) = @_; 
+	`$job`;
+}
