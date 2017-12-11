68b92f756156aa7983018065ab48c92ab5161a36
CharlieBarker <zcbtark@ucl.ac.uk>
2017-12-11 15:20:24 +0000

    updated R script, more succint, less bs

--- nLoops.R
+++ nLoops.R
@@ -1,5 +1,5 @@
 ########PATH HERE#########
-path <- "/acrm/bsmhome/zcbtark/Documents/abymod-masters-project/learnding/results/spreadsheets/nloops 10-30"
+path <- "/acrm/bsmhome/zcbtark/Documents/abymod-masters-project/learnding/results/spreadsheets/nloops 1-30"
 library(ggplot2)
 library(gtools)
 #list files in directory path
@@ -20,44 +20,39 @@ for(i in dfList[,1]) {
 	#if we're on the first iteraction, create the df we are going to add to each time
 	if(i == 1){
 		dfOriginal <- data.frame(df[ ,1], df[ ,5])
-		meanOriginal <- mean(df[ ,5])
-		sdOriginal <- sd(df[ ,5])
 		names(dfOriginal) <- c("pdb", fileName)
 	}
 	#otherwise create a new dataframe and add it to the original one. 
 	else{
 		dfAdd <- data.frame(df[ ,1], df[ ,5])
-		meanAdd <- mean(df[ ,5])
-		sdAdd <- sd(df[ ,5])
 		names(dfAdd) = c("pdb", fileName)
 		dfOriginal <- merge(dfOriginal, dfAdd, by="pdb", all = T)
-		meanOriginal <- c(meanOriginal, meanAdd)
-		sdOriginal <- c(sdOriginal, sdAdd)
 	}
 }
 
 setwd("/acrm/bsmhome/zcbtark/Documents/work")
 #print result
-
 dfOriginal <- na.omit(dfOriginal) #OMIT nas
+
 dfOriginal <- dfOriginal[!duplicated(dfOriginal),] #get rid of duplicates 
-#print (dfOriginal)
 meanOrig <- c()
+sdOrig <- c()
 for (fileName in fileList)
 {
-	name <- fileName 
-	colOriginal <- (dfOriginal[[name]])
+	colOriginal <- (dfOriginal[[fileName]])
 	meanOrig <- c(meanOrig, mean(colOriginal))
 	sdOrig <- sd(colOriginal)
 }
+
+names(dfOriginal) <- NULL
+print(dfOriginal)
+
 n <- length(fileList)
-rawData <- (dfOriginal[,2:n])
-print (rawData)
+rawData <- (dfOriginal[5:n,])
 nLoops <- c(1:n)
-sdOriginal <- sdOriginal
 #meanOriginal and meanOrig depending on whether you want to discount NA or not. 
-tableToBe <- data.frame(nLoops, meanOrig, sdOriginal)
-print (tableToBe)
+tableToBe <- data.frame(nLoops, meanOrig)
+
 pd <- position_dodge(0.1)
 ggplot(tableToBe, aes(x=nLoops, y=meanOrig)) + 
     geom_line(position=pd) +
