5ef7583bfbe9aef7483c36f6cf713133aeab5dcc
CharlieBarker <zcbtark@ucl.ac.uk>
2017-12-14 11:41:02 +0000

    fully functioning redundancy file generator

--- clustertord.pl
+++ clustertord.pl
@@ -5,12 +5,13 @@
 #   Program:    
 #   File:       clustertord.pl
 #   Date:       13.12.17
-#   Function:   takes cd-hit cluster analysis and turns it into a redundancy file.  
+#   Function:   takes cd-hit cluster analysis and turns it into a redundancy file. 
+#		You can now set the sequence similarity of your redundancy files (woo!) 
 #   Author:     Charlie Barker
 #   EMail:      zcbtark@ucl.ac.uk
-#   Usage:	
-#   Inputs: 	
-#   Outputs:
+#   Usage:	perl clustertord.pl > [nameofredundancyfile.txt]
+#   Inputs: 	the output file from your cd-hit cluster analysis
+#   Outputs:	redundancy file. 
 #               
 #*************************************************************************
 use strict; 
@@ -45,7 +46,7 @@ while(my $line = <DATA>) {
 #create empty hashes for the starter proteins and the redundant ones to follow 
 #(see the format of saba's redundancy files for reference)
 my @starters = ();
-my @redundant = ();
+my @allRedundant = ();
 #go through different proteins (and their clusters) pushing ones containing * as the starters
 for my $ele (@lines){
 	if ($ele =~ /\*/) {
@@ -53,39 +54,50 @@ for my $ele (@lines){
 	}
 	#if no * is found push into redundant array. 
 	else {
-		push @redundant, $ele;
+		push @allRedundant, $ele;
 	}
 }
-
-#create hash of starting proteins with cluster as key 
-my %starterHash =(); 
+#for every starting protein
 for my $starter (@starters){
+	#isolate the cluster no.
 	my $clusterNo  = substr $starter, 1, 11; 
+	#remove whitespace either side. 
 	$clusterNo =~ s/^\s+|\s+$//g;
-	my $pdbName = substr $starter, -15, 5; 
-	$starterHash{$clusterNo} = $pdbName;	
+	my @redundant = ();
+	#ho through all redundant proteins of the same cluster no. 
+	for my $allRedun (@allRedundant){
+		if ($allRedun =~ /$clusterNo\s/) {
+			#get the pdb code of these proteins 
+			my $char = ', >';
+			my $loc = index($allRedun, $char);
+			$loc = $loc+3;  
+			my $redunPdb = substr $allRedun, $loc, 5;
+			$redunPdb = deProcessLine($redunPdb);
+			#push into their own array 
+			push @redundant, $redunPdb;
+		}	
+	}
+	my $starterPdb = substr $starter, -15, 5;
+	$starterPdb = deProcessLine($starterPdb);
+	print "$starterPdb,";
+	for my $redun (@redundant){
+		print " $redun,";
+	}
+	print "\n";
 }
-#create hash of redundant proteins with cluster as key
-my %redundantHash; 
-my $clusterNo;
-for my $ele (@redundant){
-	#print "$ele";
-	my $char = ', >';
-	my $loc = index($ele, $char);
-	$loc = $loc+3;  
-	my $pdbName = substr $ele, $loc, 5; 
-	my $char1 = '>';
-	my $loc1 = index($ele, $char1);
-	$loc1 = $loc1+1;
-	my $clusterNo  = substr $ele, $loc1, 11;
-	$clusterNo =~ s/^\s+|\s+$//g;
-	#print "$clusterNo\n";
-	$redundantHash{$clusterNo} = $pdbName;
+
+##########SUBROUTINES##########
+#(opposite of Andrew's subroutine; ProccessLine)
+sub deProcessLine
+{
+	my($entry) = @_;
+	$entry = "\U$entry";
+	my $number = substr $entry, -1, 1;
+	$number = "$number" + 1; 
+	$entry = substr($entry, 0, -1);
+	$entry = "$entry\_$number";
+
+	return($entry);
 }
-print "$redundantHash{'Cluster 1'}\n";
-
-
-###########SUBROUTINES##########
-
 
 
