9fc6cb32c95b37e249d700e314beb7ffc28b3721
CharlieBarker <zcbtark@ucl.ac.uk>
2018-01-25 12:38:40 +0000

    energy run - to check what exactly going wrong with /var/tmp filling up

--- learnding/iterativeanalysis.pl
+++ learnding/iterativeanalysis.pl
@@ -31,14 +31,28 @@ use strict;
 #I would recomend taking 
 #Ensure you are running the tests on martin-cs00 (i.e. use "ssh
 #martin-cs00") to get on the right machine.
-#nohup [command] & (for no hangup) 
+#no hup [command] & for no hangup 
 #ALWAYS CHECK JOBS ARE RUNNING BY TYPING ps auxwww | grep abymod.pl  AFTER EXECUTION
+		
 
 
-my $job1 = "perl analyseabYmod.pl 6arestraintloopdb -noopt -loopdb 2> results/abyModSTDERR/6arestraintloopdb.txt";  
-my $job2 = "perl analyseabYmod.pl 6arestraintnoloopdb -noopt -noloopdb 2> results/abyModSTDERR/6arestraintnoloopdb.txt"; 
-my $job3 = "perl analyseabYmod.pl 6arestraintdefault -noopt 2> results/abyModSTDERR/6arestraintdefault.txt"; 
 
+my $job1 = "perl analyseabYmod.pl default -noopt 2> results/abyModSTDERR/default.txt";  
+my $job2 = "perl analyseabYmod.pl energynLoops1 -noopt -loopdb -nloophits=1 2> results/abyModSTDERR/energynLoops1.txt"; 
+my $job3 = "perl analyseabYmod.pl energynLoops2 -noopt -loopdb -nloophits=2 2> results/abyModSTDERR/energynLoops2.txt"; 
+my $job4 = "perl analyseabYmod.pl energynLoops3 -noopt -loopdb -nloophits=3 2> results/abyModSTDERR/energynLoops3.txt"; 
+my $job5 = "perl analyseabYmod.pl energynLoops4 -noopt -loopdb -nloophits=4 2> results/abyModSTDERR/energynLoops4.txt"; 
+my $job6 = "perl analyseabYmod.pl energynLoops5 -noopt -loopdb -nloophits=5 2> results/abyModSTDERR/energynLoops5.txt"; 
+my $job7 = "perl analyseabYmod.pl energynLoops6 -noopt -loopdb -nloophits=6 2> results/abyModSTDERR/energynLoops6.txt"; 
+my $job8 = "perl analyseabYmod.pl energynLoops7 -noopt -loopdb -nloophits=7 2> results/abyModSTDERR/energynLoops7.txt"; 
+my $job9 = "perl analyseabYmod.pl energynLoops8 -noopt -loopdb -nloophits=8 2> results/abyModSTDERR/energynLoops8.txt"; 
+my $job10 = "perl analyseabYmod.pl energynLoops9 -noopt -loopdb -nloophits=9 2> results/abyModSTDERR/energynLoops9.txt"; 
+my $job11 = "perl analyseabYmod.pl energynLoops10 -noopt -loopdb -nloophits=10 2> results/abyModSTDERR/energynLoops10.txt"; 
+my $job12 = "perl analyseabYmod.pl energynLoops11 -noopt -loopdb -nloophits=11 2> results/abyModSTDERR/energynLoops11.txt";
+my $job13 = "perl analyseabYmod.pl energynLoops12 -noopt -loopdb -nloophits=12 2> results/abyModSTDERR/energynLoops12.txt";
+my $job14 = "perl analyseabYmod.pl energynLoops13 -noopt -loopdb -nloophits=13 2> results/abyModSTDERR/energynLoops13.txt";
+my $job15 = "perl analyseabYmod.pl energynLoops14 -noopt -loopdb -nloophits=14 2> results/abyModSTDERR/energynLoops14.txt";
+my $job16 = "perl analyseabYmod.pl energynLoops15 -noopt -loopdb -nloophits=15 2> results/abyModSTDERR/energynLoops15.txt";
 
 
 
@@ -48,8 +62,9 @@ my $job3 = "perl analyseabYmod.pl 6arestraintdefault -noopt 2> results/abyModSTD
 
 #####################################################################################################
 
-my @jobs = ($job1, $job2, $job3);		
-
+my @jobs = ($job2, $job3, $job4, $job5, $job6, $job7, $job8, $job9, $job10, $job11, $job12,
+	$job13, $job14, $job15, $job16);
+@jobs = reverse @jobs;
 #go through jobs running them while the total number of abymods that are running is less than 4
 foreach my $job (@jobs)
 {
