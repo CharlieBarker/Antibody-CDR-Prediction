60017725a1f675fd465721dcac578042affc96e1
CharlieBarker <zcbtark@ucl.ac.uk>
2018-02-06 14:43:30 +0000

    added subroutine that isolate h3 and framework from pdb files NEED TO CHECK WHETHER ALL THE LOOPS ARE THE SAME LENGTH

--- machine learning/findtemplateused.pl
+++ machine learning/findtemplateused.pl
@@ -81,10 +81,14 @@ if(!open(SPREAD, "<$filePath"))
 my @redundant;
 #put the pdb files and the loop name of the template used for CDRH3 together
 for(my $i=0; $i<$no; $i++) {
+	#open spreadsheet file (for some reason i have to do this with each
+	# iteration)
 	open(SPREAD, "<$filePath"); 
 	my $name = $names[$i];
 	$name = "$name.pdb";
 	my $loop = $loops[$i];
+	# go through spreadsheet, and keep the pdb name and template loop name if 
+	#the name exists in the spreadsheet. 
 	while(my $line = <SPREAD>) {
 		my @entries = split(/\t+/, $line);
 		if($name eq $entries[0]){
@@ -97,13 +101,84 @@ for(my $i=0; $i<$no; $i++) {
 
 
 
-my $num = @redundant;
-
-foreach my $element (@redundant){
-	print "$element\n";
-}
-print "$num";
+#my $num = @redundant;
+#
+#foreach my $element (@redundant){
+#	print "$element\n";
+#}
+#print "$num";
 
 #my @out = util::PdbToSeq($loop, $path);
 #print "@out";
+
+my $pdb = "3d9a0.pdb";
+my @out = CDRH3seq($pdb);
+print @out;
+
+#*************************************************************************
+#> CDRH3seq($pdb)
+#  ----------------------------------------------
+#  Inputs:   \scalar  $pdb	Scalar containing antibody pdbname in the standard
+#				PDB format
+# 						
+#
+#  returns array of amino acid one letter sequence including the 3 residue shoulder 
+#  sequence each side
+#
+#  06.02.2018 by C.G.B.
+
+sub CDRH3seq
+{
+	my($pdb) = @_;
+	my $path = "/acrm/bsmhome/zcbtark/Documents/abymod-masters-project/abymod/DATA/abpdblib";
+	my $pdbPath = "$path/$pdb";
+	open(FILE, "<$pdbPath"); 
+	if(!open(FILE, "<$pdbPath"))
+	{
+	    print STDERR "Error: unable to open file $pdbPath\n";
+	    exit 1;
+	}
+	my @res;
+	#step through lines in PDB loop file
+	while(my $line = <FILE>){
+		my @entries = split(/\s+/, $line);
+		#only push if their on heavy chain and an atom. 
+		if($entries[0] == "ATOM" && $entries[4] eq "H"){
+			#add the residue name and the residue number
+			push @res, "$entries[3]" . " $entries[5]";
+		}
+
+	}
+	#remove repeated values 
+	@res = util::uniq(@res);
+	my $resNo = @res;
+	my $start;
+	my $end;
+	#get indexes of start and end.. minus and add three respectively to include framework
+	for(my $i=0; $i<$resNo; $i++) {
+		my @entries = split(/\s+/, $res[$i]);
+		if($entries[1] == 95){
+			$start = $i-3;
+		}
+		if($entries[1] == 102){
+			$end = $i+3;
+		}
+	}
+	#isolate array between start and end scalars. 
+	my @cdrh3 = @res[$start..$end];
+	my @threeCode;
+	#isolate three letter code 
+	foreach my $residue (@cdrh3){
+		my @entries = split(/\s+/, $residue);
+		push @threeCode, $entries[0];
+	}
+	my @out;
+	#translate code to one letter code 
+	foreach my $ele (@threeCode){
+		my $translatedCode = $util::throneData{$ele};
+		push @out, $translatedCode;
+	}
+	return @out;
+}
+
 
