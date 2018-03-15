b0c058a137c4c51d243399ab8dfa2d80f142a1ef
CharlieBarker <zcbtark@ucl.ac.uk>
2018-03-15 15:37:52 +0000

    added modelrunner to cycle through all the models for the different thresholds

---
+++ QualityPredictor/config.pm
@@ -0,0 +1,18 @@
+package config;
+use strict;
+
+#*************************************************************************
+#
+#   Program:    QualityPredictor
+#   File:       config.pm
+#   Date:       15.03.2018
+#   Function:   Config file for QualityPredictor program
+#   Author:     Charlie Barker
+#               
+#*************************************************************************
+
+#Root directory for QualityPredictor
+$config::qpRoot='/acrm/bsmhome/zcbtark/Documents/abymod-masters-project/QualityPredictor'; 
+
+#machine learning models 
+$config::mlModels="$config::qpRoot/models"; 
